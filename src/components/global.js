da588746ebed59e9c17ae1e178f8325536ef3ee4
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2022-10-10 20:32:30 +0300

    feat: createContext()

--- src/components/global.js
+++ src/components/global.js
@@ -1,4 +1,5 @@
 import { Outlet, Link } from 'react-router-dom';
+import AuthConsumer from '../hooks/auth';
 
 export const LoginPage = () => {
   return (
@@ -9,6 +10,8 @@ export const LoginPage = () => {
 };
 
 export const HomePage = () => {
+  const auth = AuthConsumer();
+  console.log(auth);
   return (
     <main>
       <h1 className="text-center my-3 bg-yellow-400 text-gray-700 w-40 mx-auto rounded">
